d2eb056dee60821315cab8e325ffa1452a3d7571
nkina1516 <157325721+nkina1516@users.noreply.github.com>
2025-02-22 22:34:29 -0600

    added some files

--- Assets/Scripts/Slingshot.cs
+++ Assets/Scripts/Slingshot.cs
@@ -75,6 +75,7 @@ public class Slingshot : MonoBehaviour
             projRB.isKinematic = false;                                       
             projRB.collisionDetectionMode = CollisionDetectionMode.Continuous;
             projRB.velocity = -mouseDelta * velocityMult;
+            FollowCam.POI = projectile;
             projectile = null;                                                
         }
      }
